4247ba8a404fd0c7647ca4122a1866d0d195aa7c
zhou <1042883146@qq.com>
2017-11-29 23:08:16 +0800

    props

---
+++ 08-02/src/js/components/bodyIndex.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class BodyIndex extends React.Component {
+  constructor() {
+    super()//调用基类的所有初始化方法
+    this.state = {
+      userName: 'zhoushuai'
+    }
+  }
+  componentWillMount() {
+    // 定义你的逻辑即可
+    console.log('这是bodyIndex页面')
+  }
+  componentDidMount() {
+    console.log('这是这是bodyIndex页面页面加载完成')
+  }
+  render() {
+    // 注释
+    /* 注释 */
+    setTimeout(() => {
+      this.setState({userName:'zhangwenqing'})
+    }, 3000)
+    return (
+      <div>
+        <h2>这是主题内容部分</h2>
+        <p>{this.state.userName}</p>
+        {/* props属于外来属性 */}
+        {/* state属于模块滋生属性 */}
+        <p>{this.props.userName}{this.props.userId}</p>
+      </div>
+    )
+  }
+}
+
+export default BodyIndex
