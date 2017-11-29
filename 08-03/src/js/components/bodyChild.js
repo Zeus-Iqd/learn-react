ef5d834fb8157b45f08d2ff8f80c1412e71ad170
zhou <1042883146@qq.com>
2017-11-29 23:29:12 +0800

    子组件向父组件传递数据

---
+++ 08-03/src/js/components/bodyChild.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export default class BodyChild extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* 子页面通过调用事情处理父组件的方法 */}
+        <p>子页面输入：<input type="text" onChange={this.props.handle}/></p>
+      </div>
+    )
+  }
+}
---
+++ 08-03/src/js/components/bodyIndex.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import BodyChild from './bodyChild'
+
+class BodyIndex extends React.Component {
+  constructor() {
+    super()//调用基类的所有初始化方法
+    this.state = {
+      userName: 'zhoushuai'
+    }
+  }
+  componentWillMount() {
+    // 定义你的逻辑即可
+    console.log('这是bodyIndex页面')
+  }
+  componentDidMount() {
+    console.log('这是这是bodyIndex页面页面加载完成')
+  }
+  changeUserInfo() {
+    this.setState({ userName: '张文庆' })
+  }
+  handleChildValueChange(event) {
+    this.setState({ userName: event.target.value })
+  }
+  render() {
+    // 注释
+    /* 注释 */
+    // setTimeout(() => {
+    //   this.setState({userName:'zhangwenqing'})
+    // }, 3000)
+    return (
+      <div>
+        <h2>这是主题内容部分</h2>
+        <p>{this.state.userName}</p>
+        {/* props属于外来属性 */}
+        {/* state属于模块滋生属性 */}
+        <p>{this.props.userName}{this.props.userId}</p>
+        <input type="button" value="按钮" onClick={this.changeUserInfo.bind(this)} />
+        <BodyChild handle={this.handleChildValueChange.bind(this)}/>
+      </div>
+    )
+  }
+}
+
+export default BodyIndex
